884c81127955037e28f27f3c3048ecd4291eec6a
VilkasM <a01733749@itesm.mx>
2021-04-28 12:11:19 -0500

    cambio de version

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const PORT  = process.env.PORT || 8117; 
+const PORT  = process.env.PORT || 3300; 
 
 server.listen(PORT, () => console.log(PORT));
