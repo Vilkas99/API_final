173d5749f617087d8a664ce7e07b0bc93be42299
VilkasM <a01733749@itesm.mx>
2021-04-27 20:26:41 -0500

    Modelo para tabla Usuarios

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const PORT  = process.env.PORT || 8116; 
+const PORT  = process.env.PORT || 8117; 
 
 server.listen(PORT, () => console.log("El servidor está corriendo"));
